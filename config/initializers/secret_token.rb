db5a23a56f95606754630740700e2a387c413445
Test <andrew@Andrews-MacBook-Pro.local>
2014-03-18 22:31:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rightcare::Application.config.secret_key_base = '97259d26c1dabb87c5fbe75899e3071ff563aa4433c2a594630c7ea908256cea86673261e16b19d887f68cb87496eb6554c0c503252e7f0da29379d1db405d6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rightcare::Application.config.session_store :cookie_store, key: '_rightcare_session'
